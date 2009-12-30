4a5b46bc160b9a947b780314b597ee8d635558d5
Bart Spaans <onderstekop@gmail.com>
2009-12-30 22:51:18 +0100

    Added groupings and (time) tables to Report

--- report/Report.hs
+++ report/Report.hs
@@ -16,6 +16,8 @@ import Stats
 import Text.PrettyPrint
 import Control.Arrow
 import Control.Applicative
+import Data.List
+import Data.Function
 
 
 -- Type Synonyms — Reports
@@ -87,7 +89,7 @@ fromIR (IR ir) = ir
 
 -- Algebra and Calendar fold applied — report
 --
-report        :: Report SIndexedReport
+report        :: SReport
 indexAlgebra  :: CalendarSAlgebra SIndexedY SIndexedM SIndexedD SIndexedReport
 indexFromFile :: FilePath -> IO StatOptions -> IO SIndexedReport
 
@@ -103,3 +105,53 @@ indexAlgebra   = (IR, y, mon, days)
 
 indexFromFile f so  = report <$> fromFile f so 
 
+
+
+-- Groupings
+-- The following functions do grouping on 
+-- the methods in EditStats. For grouping 
+-- on Edits see Edits.hs.
+--
+type Grouping = CalendarS -> [Stats]
+
+grouping :: Ord a => (EditStats -> a) -> Grouping
+grouping f = groupWith f . sortBy (compare `on` f) . concat  .  flatten
+
+groupExtensions :: Grouping
+groupLanguages  :: Grouping
+groupProjects   :: Grouping
+groupFilenames  :: Grouping
+groupEditTimes  :: Grouping
+
+groupExtensions = grouping extInformation
+groupLanguages  = grouping language
+groupProjects   = grouping project
+groupFilenames  = grouping fileName
+groupEditTimes  = grouping editTime
+
+
+-- Tables
+-- Generates time tables from the groupings.
+--
+type Table a b = [(a, b)]
+type TimeTable a = [(a, Time)]
+type Time = (Int, Int, Int)
+
+table           :: Ord a => (EditStats -> a) -> Report (TimeTable a)
+tableExtensions :: Report (TimeTable (Maybe Description))
+tableLanguages  :: Report (TimeTable (Maybe (Description, String)))
+tableProjects   :: Report (TimeTable (Maybe (Description, String)))
+tableFilenames  :: Report (TimeTable FilePath)
+sumTime         :: Stats -> (Int, Int, Int)
+
+
+table f = map (f . head &&& sumTime) . grouping f 
+tableExtensions = table extInformation
+tableLanguages  = table language
+tableProjects   = table project
+tableFilenames  = table fileName
+
+
+sumTime = fromSeconds . sum . map (toSeconds . editTime)
+  where toSeconds (h, m, s) = h * 3600 + m * 60 + s
+        fromSeconds s = (div s 3600, div (mod s 3600) 60, mod (mod s 3600) 60)
