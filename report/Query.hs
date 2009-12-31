16aaf7c6af1ecba64b8b71c48819976242313075
Bart Spaans <onderstekop@gmail.com>
2009-12-31 09:52:55 +0100

    Added grammar description and examples to Query

--- report/Query.hs
+++ report/Query.hs
@@ -11,13 +11,12 @@ import Data.Function
 
 import Maybe
 
-
 type Queries = [Query]
-type Query = (View, Constraint, Group)
+type Query = [QueryPart]
+type QueryPart = (View, Constraint, Group)
 type Constraint = Stats -> (Stats, Stats)
 type View = EditStats -> String
 type Group = Stats -> [Stats]
-
 
 
 
@@ -30,16 +29,51 @@ makeConstraint p = con ([], [])
 langQ = (showLanguage "NONE" . language, makeConstraint (isJust . language), groupWith language)
 
 
-makeTree :: Stats -> Queries -> StatsTree
+makeTree :: Stats -> Query -> StatsTree
 makeTree s q = Root (makeTree' s q (0,0,0))
                   
-
-makeTree' :: Stats -> Queries -> Time -> [StatsTree]
+-- TODO: add sorting and totals?
+makeTree' :: Stats -> Query -> Time -> [StatsTree]
 makeTree' s []         t  = [Leaf t]
 makeTree' s ((v,c, g):cs) t = map (\gr -> makeNode (v . head $ gr) (sumTime gr) yes cs) (g . sortBy (compare `on` v) $ yes)
                                       ++ [makeNode "None" (sumTime no) no cs]
    where (yes, no) = c s
 
-makeNode :: String -> Time -> Stats -> Queries -> StatsTree
+makeNode :: String -> Time -> Stats -> Query -> StatsTree
 makeNode s t yes cs = Node 0 s (makeTree' yes cs t)
 
+{-
+ 
+ Examples
+
+ Constraint Language:
+
+   1.   language * extension
+   2.   group language * extension
+   3.   group language * group extension
+   4.   group language limit 5
+   5.   group language ascending
+
+ in (pseudo?) SQL: 
+    
+   1. SELECT language, extension, editTime FROM stats
+   2. SELECT language, extension, sum(editTime) FROM stats GROUP BY language
+   3. SELECT language, extension, sum(editTime) FROM stats GROUP BY language, extension
+   4. SELECT language, sum(editTime) FROM stats GROUP BY language LIMIT 5
+   5. SELECT language, sum(editTime) AS e FROM stats GROUP BY language ORDER BY e ASC
+ 
+ Grammar:
+
+   The terminals are not case sensitive, so `GROUP' is the same as `group'
+
+   QUERY   := ε | Q QPREFIX?
+   Q       := T | Q * T
+   T       := group? TABLE 
+   QPREFIX := LIMIT | ORDER
+   LIMIT   := limit DIGIT+
+   ORDER   := ascending | descending | asc | desc
+   TABLE   := extension | language | project | filename | year | month | day | dow | doy 
+
+
+
+ -}
