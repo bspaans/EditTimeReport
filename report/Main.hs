6685e0c266a2dee81da03e82dc2ba58b70c9034a
Bart Spaans <onderstekop@gmail.com>
2010-01-02 20:40:55 +0100

    Updated Main program and Readme

--- report/Main.hs
+++ report/Main.hs
@@ -18,13 +18,11 @@ main = do hSetBuffering stdout NoBuffering  -- remove LineBuffering from stdout
           if length args < 1 
             then usage
             else do so <- askStatOptions
-                    if length args == 1 then htmlFromFile (head args) po so >>= putStrLn
+                    let iqueries = do s <- statsFromFile (head args) so ; interactiveQueries s
+                    if length args == 1 then iqueries
                                         else if length args == 2 && args !! 1 /= "-i" 
                                                then htmlFromFile (head args) po so >>= writeFile (args !! 1)
-                                               else do s <- statsFromFile (head args) so
-                                                       interactiveQueries s
-
-
+                                               else iqueries
 
 usage = do putStrLn "Report generator"
            putStrLn "Copyright 2009, Bart Spaans"
